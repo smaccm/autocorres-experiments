ea26e99ef2216a09a5761441c3df64c573208d61
Daniel DaCosta <chaosape@chaosape.com>
2016-08-01 18:33:07 -0500

    Max_DWD proof is very simple now.

--- max/Max_DWD.thy
+++ max/Max_DWD.thy
@@ -6,20 +6,8 @@ install_C_file max_DWD.c
 autocorres [ heap_abs_syntax, ts_rules = nondet, unsigned_word_abs = max ] max_DWD.c
 context max_DWD begin
 
-inductive array :: "lifted_globals \<Rightarrow> 32 word ptr \<Rightarrow> nat \<Rightarrow> bool"  where
-  one:"is_valid_w32 h p \<Longrightarrow> array h p (Suc 0)"
-| more:"\<lbrakk>is_valid_w32 h p; array h (p +\<^sub>p 1) n\<rbrakk> \<Longrightarrow> array h p (Suc n)"
-
-lemma ptr_distrib[simp]:"p +\<^sub>p x +\<^sub>p y = p +\<^sub>p (x + y)" 
-  by (simp add: CTypesDefs.ptr_add_def distrib_right)
-
-lemma all_valid_raw:"array h p l \<Longrightarrow> \<forall> i<l. is_valid_w32 h (p +\<^sub>p int i)"
-apply(induction rule:array.induct)
-apply auto
-using less_Suc_eq_0_disj by auto
-
-lemma all_valid:"\<lbrakk>array h p l; i < l\<rbrakk> \<Longrightarrow> is_valid_w32 h (p +\<^sub>p int i)" 
-  using all_valid_raw by auto
+definition array :: "lifted_globals \<Rightarrow> 32 word ptr \<Rightarrow> nat \<Rightarrow> bool"  where
+ "array h p l \<equiv> (\<forall> i < l. is_valid_w32 h (p +\<^sub>p int i))"
 
 lemma "
 \<lbrace> \<lambda> (h::lifted_globals). 
@@ -30,7 +18,7 @@ lemma "
 \<lbrace> \<lambda> (m::nat) (h::lifted_globals). 
   h' = h 
   \<and> (\<forall> j<l. unat h'[p +\<^sub>p int j] \<le> m) 
-  \<and> (\<exists> k<l. unat h'[p +\<^sub>p int k] = m)\<rbrace>!
+  \<and> 0 < l \<longrightarrow> (\<exists> k<l. unat h'[p +\<^sub>p int k] = m)\<rbrace>!
 "
 apply (rule validNF_assume_pre)
 apply (unfold max'_def)
@@ -44,6 +32,5 @@ apply (subst whileLoop_add_inv
       \<and> (i = 0 \<longrightarrow> 0 = m)"
     and M = "\<lambda>((i,m),_).  l - i"])
 apply wp
-apply (insert all_valid less_Suc_eq)
-apply (auto simp add: uint_nat unat_less_helper diff_less_mono2 word_1_0 not_less_iff_gr_or_eq)
-by (meson Zero_not_Suc max_DWD.array.cases)
+apply (auto simp add: array_def less_Suc_eq)
+done
